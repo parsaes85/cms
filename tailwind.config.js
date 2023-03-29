bf61de8acb83fba2688e7ff6f6a260256ab35eab
parsa <parsaes771385@gmail.com>
2023-03-30 02:03:16 +0430

    initial project

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html', './Pages/*/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'lightBlue': '#1943da',
+        'darkBlue': '#17203f',
+      },
+
+      boxShadow: {
+        'custom': '2px 1px 10px rgba(0, 0, 0, .2)'
+      }
+    },
+
+    fontFamily: {
+      vazir: ["vazir"],
+      lalezar: ["lalezar"]
+    },
+  },
+  plugins: [],
+}
+
