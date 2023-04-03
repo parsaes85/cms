80b3790ca805b37c3d43e5d3907e0fff964c8061
parsa <parsaes771385@gmail.com>
2023-04-03 21:14:34 +0430

    fix sidebar responsive

--- Pages/Main/js/script.js
+++ Pages/Main/js/script.js
@@ -4,8 +4,8 @@ const sidebarOpenBtn = $.querySelector('.sidebar-open-btn')
 const sidebar = $.getElementById('sidebar')
 
 sidebarOpenBtn.addEventListener('click', () => {
-    sidebar.classList.remove('-right-96')
+    sidebar.classList.replace('-right-96', 'right-0')
 })
 sidebarCloseBtn.addEventListener('click', () => {
-    sidebar.classList.add('-right-96')
+    sidebar.classList.replace('right-0', '-right-96')
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html', './Pages/*/index.html'],
+  content: ['./*.html', './Pages/*/*.{html,js}'],
   theme: {
     extend: {
       colors: {
