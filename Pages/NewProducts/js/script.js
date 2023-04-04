214702cc22993f5a01515218903822aa18fef5ed
parsa <parsaes771385@gmail.com>
2023-04-05 00:38:45 +0430

    add Add Product

---
+++ Pages/NewProducts/js/script.js
@@ -0,0 +1,16 @@
+let $ = document
+const sidebarCloseBtn = $.querySelector('.sidebar-close-btn')
+const sidebarOpenBtn = $.querySelector('.sidebar-open-btn')
+const sidebar = $.getElementById('sidebar')
+
+sidebarOpenBtn.addEventListener('click', () => {
+    sidebar.classList.replace('-right-96', 'right-0')
+})
+sidebarCloseBtn.addEventListener('click', () => {
+    sidebar.classList.replace('right-0', '-right-96')
+})
+
+Toast.fire({
+    icon: 'success',
+    title: 'محصول با موفقیت اضافه شد',
+})
